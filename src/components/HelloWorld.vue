b43a0121310c45252f2e243d661ad0b10283b11e
onesimu <872634214@qq.com>
2022-06-06 15:14:08 +0800

    format

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,52 +1,43 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+  import { ref, reactive } from 'vue'
 
-defineProps<{ msg: string }>()
+  const state = reactive({
+    weatherList: []
+  })
 
-const count = ref(0)
+  fetch('http://wthrcdn.etouch.cn/weather_mini?city=北京').then(r => r.json())
+    .then(r => {
+      state.weatherList = r.data.forecast
+    })
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-
-  <p>
-    Recommended IDE setup:
-    <a href="https://code.visualstudio.com/" target="_blank">VS Code</a>
-    +
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-  </p>
-
-  <p>See <code>README.md</code> for more information.</p>
-
-  <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">
-      Vite Docs
-    </a>
-    |
-    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Docs</a>
-  </p>
-
-  <button type="button" @click="count++">count is: {{ count }}</button>
-  <p>
-    Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
-  </p>
+  <ul class="weather_list">
+    <li v-for="item of state.weatherList">
+      {{item.data}}
+      {{item.high}}
+      {{item.fengli}}
+      {{item.low}}
+      {{item.fengxiang}}
+      {{item.type}}
+    </li>
+  </ul>
 </template>
 
 <style scoped>
-a {
-  color: #42b983;
-}
+  a {
+    color: #42b983;
+  }
 
-label {
-  margin: 0 0.5em;
-  font-weight: bold;
-}
+  label {
+    margin: 0 0.5em;
+    font-weight: bold;
+  }
 
-code {
-  background-color: #eee;
-  padding: 2px 4px;
-  border-radius: 4px;
-  color: #304455;
-}
+  code {
+    background-color: #eee;
+    padding: 2px 4px;
+    border-radius: 4px;
+    color: #304455;
+  }
 </style>
